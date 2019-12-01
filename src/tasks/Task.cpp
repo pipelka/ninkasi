368a53e75b39befdc2253ffc99c7a5dca5951b81
Alexander Pipelka <alexander.pipelka@gmail.com>
2019-12-01 16:44:33 +0100

    [Task] relay switch checking

--- src/tasks/Task.cpp
+++ src/tasks/Task.cpp
@@ -19,6 +19,7 @@ void Task::loop() {
 void Task::clear() {
     m_atom.done = false;
     m_atom.elapsed = 0;
+    m_atom.sw_on = 0;
     m_startMillis = 0;
 }
 
@@ -31,14 +32,34 @@ void Task::switchOn(bool on, bool force) {
         return;
     }
 
-    m_atom.sw_on = on;
+    bool done = false;
+    int count = 0;
 
-    if(m_atom.sw_on == 1) {
-        m_relay->setSingle(1 << m_atom.sw);
+    while(!done) {
+        if(on == 1) {
+            done = m_relay->setSingle(1 << m_atom.sw);
+        }
+        else {
+            done = m_relay->delSingle(1 << m_atom.sw);
+        }
+
+        count++;
+
+        if(!done) {
+            delay(1000);
+            m_relay->setup();
+        }
+
+        if(!done && count == 10) {
+            Serial.print("[ERROR] RelayBus NOT responding !!!");
+            // TODO - send notification, switch off device, ...
+            // just take every possible action to avoid an unknown relay
+            // state (e.g. heating forever, ...)
+            return;
+        }
     }
-    else {
-        m_relay->delSingle(1 << m_atom.sw);
-    }
+
+    m_atom.sw_on = on;
 }
 
 void Task::serialize() {
