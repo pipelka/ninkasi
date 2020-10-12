f8019f2400522258970e99eb044e586bea8d8b65
Alexander Pipelka <alexander.pipelka@gmail.com>
2020-10-12 14:31:25 +0200

    [TaskHoldStir] added new class

---
+++ src/tasks/TaskHoldStir.cpp
@@ -0,0 +1,32 @@
+#include "TaskHoldStir.h"
+
+void TaskHoldStir::loop() {
+    // stir disabled ?
+    if(m_stirInterval == 0) {
+        TaskHold::loop();
+        return;
+    }
+
+    uint8_t e = elapsed();
+
+    // check if we should start the stir (every 'stir interval' minutes)
+    if(e % m_stirInterval == 0 && !isSwitchOn() && e > 0) {
+        m_stirStart = e;
+        switchOn(on());
+        return;
+    }
+
+    // stop stirring after a minute
+    if(e >= m_stirStart + 1) {
+        m_stirStart = 0;
+    }
+
+    // business as usual when not stirring
+    if(m_stirStart == 0) {
+        TaskHold::loop();
+    }
+}
+
+void TaskHoldStir::setInterval(uint8_t interval) {
+    m_stirInterval = interval;
+}
---
+++ src/tasks/TaskHoldStir.h
@@ -0,0 +1,15 @@
+#include "TaskHold.h"
+
+class TaskHoldStir : public TaskHold {
+public:
+
+    void loop() override;
+
+    void setInterval(uint8_t interval);
+    
+protected:
+
+    uint8_t m_stirInterval{17};
+
+    uint8_t m_stirStart{0};
+};
