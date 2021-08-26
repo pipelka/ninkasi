6f7cd68fe57d0df08521525278b7d4ab434712ac
Alexander Pipelka <alexander.pipelka@gmail.com>
2021-08-26 22:06:11 +0200

    [TaskHoldStir] logic fixes

--- src/tasks/TaskHoldStir.cpp
+++ src/tasks/TaskHoldStir.cpp
@@ -10,21 +10,31 @@ void TaskHoldStir::loop() {
     uint8_t e = elapsed();
 
     // check if we should start the stir (every 'stir interval' minutes)
-    if(e % m_stirInterval == 0 && !isSwitchOn() && e > 0) {
+    if((e % m_stirInterval) == 0 && !isSwitchOn() && e > 0 && m_stirStart == 0) {
         m_stirStart = e;
         switchOn(on());
         return;
     }
 
     // stop stirring after a minute
-    if(e >= m_stirStart + 1) {
+    if(e >= m_stirStart + 1 && m_stirStart != 0) {
         m_stirStart = 0;
+
+        if(shouldStop()) {
+            switchOn(!on());
+            return;
+        }
     }
 
     // business as usual when not stirring
     if(m_stirStart == 0) {
+        Serial.println("resting ...");
         TaskHold::loop();
+        return;
     }
+
+    Serial.println("stirring ...");
+    Task::loop();
 }
 
 void TaskHoldStir::setInterval(uint8_t interval) {
